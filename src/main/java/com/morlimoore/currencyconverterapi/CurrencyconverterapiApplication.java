835bacb1d89e8d6b0055360acf021d3775d8a2be
morlimoore <cooliyk2@gmail.com>
2021-01-13 16:21:48 +0100

    Initial commit: Project start up

---
+++ src/main/java/com/morlimoore/currencyconverterapi/CurrencyconverterapiApplication.java
@@ -0,0 +1,13 @@
+package com.morlimoore.currencyconverterapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyconverterapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyconverterapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/morlimoore/currencyconverterapi/CurrencyconverterapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morlimoore.currencyconverterapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyconverterapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
