a5f30cb3eb83f44ad181d9f14b510baaf16fceb8
morlimoore <cooliyk2@gmail.com>
2021-01-14 20:11:42 +0100

    Adds user functionality

---
+++ src/main/java/com/morlimoore/currencyconverterapi/service/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.morlimoore.currencyconverterapi.service.impl;
+
+import com.morlimoore.currencyconverterapi.entities.User;
+import com.morlimoore.currencyconverterapi.entities.UserDetailsImpl;
+import com.morlimoore.currencyconverterapi.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+	@Autowired
+	UserRepository userRepository;
+
+	@Override
+	@Transactional
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.findByUsername(username)
+				.orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+		return UserDetailsImpl.build(user);
+	}
+
+}
