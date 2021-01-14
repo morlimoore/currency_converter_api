aa7ff7a3ae1ecb37ae114d4beed365563fcee61c
morlimoore <cooliyk2@gmail.com>
2021-01-14 20:11:14 +0100

    Adds role functionality

---
+++ src/main/java/com/morlimoore/currencyconverterapi/entities/Role.java
@@ -0,0 +1,25 @@
+package com.morlimoore.currencyconverterapi.entities;
+
+import com.morlimoore.currencyconverterapi.util.RoleEnum;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "roles")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Role {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Enumerated(EnumType.STRING)
+	@Column(length = 20, nullable = false)
+	private RoleEnum name;
+}
