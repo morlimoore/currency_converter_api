04a5a4eaeb72140234e5b9a7e0fbf1ca1dff9c99
morlimoore <cooliyk2@gmail.com>
2021-01-16 17:58:45 +0100

    Refactoring

--- src/main/java/com/morlimoore/currencyconverterapi/util/CreateResponse.java
+++ src/main/java/com/morlimoore/currencyconverterapi/util/CreateResponse.java
@@ -2,9 +2,9 @@ package com.morlimoore.currencyconverterapi.util;
 
 import com.morlimoore.currencyconverterapi.payload.ApiResponse;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.OK;
 
 public class CreateResponse {
 
@@ -12,10 +12,18 @@ public class CreateResponse {
         return new ResponseEntity<>(response, response.getStatus());
     }
 
-    public static ResponseEntity<ApiResponse<String>> bindingResultError(BindingResult result) {
+    public static ResponseEntity<ApiResponse<String>> errorResponse(String message) {
         ApiResponse<String> response = new ApiResponse<>();
         response.setStatus(BAD_REQUEST);
-        response.setMessage(result.getFieldError().getDefaultMessage());
+        response.setMessage(message);
         return createResponse(response);
     }
+
+    public static ResponseEntity<ApiResponse<String>> successResponse(String message) {
+        ApiResponse<String> response = new ApiResponse<>();
+        response.setStatus(OK);
+        response.setMessage(message);
+        return createResponse(response);
+    }
+
 }
