73dc14143c12d4374d7530daa717a45bb10fceb5
morlimoore <cooliyk2@gmail.com>
2021-01-18 10:44:15 +0100

    Handles badCredentialsException

--- src/main/java/com/morlimoore/currencyconverterapi/exceptions/RestApiExceptionHandler.java
+++ src/main/java/com/morlimoore/currencyconverterapi/exceptions/RestApiExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -46,7 +47,7 @@ public class RestApiExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler({ NoRouteToHostException.class })
     public ResponseEntity<ApiResponse> handleNoRouteToHostException(Exception ex) {
         ApiResponse response = new ApiResponse();
-        response.setMessage("Please check your internet connection");
+        response.setMessage("Error reaching server. Please check your internet connection");
         response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR);
         return createResponse(response);
     }
@@ -56,6 +57,14 @@ public class RestApiExceptionHandler extends ResponseEntityExceptionHandler {
         ApiResponse response = new ApiResponse();
         response.setMessage("There was an error reaching the server, please try again.");
         response.setStatus(HttpStatus.SERVICE_UNAVAILABLE);
+        return createResponse(response);
+    }
+
+    @ExceptionHandler({ BadCredentialsException.class })
+    public ResponseEntity<ApiResponse> handleBadCredentialsException(Exception ex) {
+        ApiResponse response = new ApiResponse();
+        response.setMessage("Username or password is invalid. Check and try again");
+        response.setStatus(HttpStatus.BAD_REQUEST);
         return createResponse(response);
     }
 
